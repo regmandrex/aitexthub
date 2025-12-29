725cf7bfaeb6943280f6c4518d47efe9416d842c
regmandrex <regmandrex@gmail.com>
2025-12-29 02:33:08 +0100

    feat: add adsense script

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8764610479002120"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  );
+}
