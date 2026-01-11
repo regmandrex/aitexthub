dc62796c7710ae747a956fa9947f96e78448f33e
regmandrex <regmandrex@gmail.com>
2026-01-11 19:58:18 +0100

    Update copyright year to 2026 in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -168,7 +168,7 @@ export default function Footer() {
         </div>
 
         <div className="mt-6 text-center text-xs text-slate-600">
-          © 2025{' '}
+          © 2026{' '}
           <Link href="https://gptcleanuptools.com" className="font-semibold text-slate-800 hover:underline">
             GPT Cleanup Tools
           </Link>{' '}
